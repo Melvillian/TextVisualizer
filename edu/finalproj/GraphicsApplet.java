6c262aed7d5cc5c6216f404fec259b6ae9edb5d9
Mari Bennett <mbennett@hmc.edu>
2014-12-06 12:05:33 -0800

    Updated GraphicsApplet code: includes code for loading pdf, and does part of the staging for getting word-color mappings.

--- edu/finalproj/GraphicsApplet.java
+++ edu/finalproj/GraphicsApplet.java
@@ -1,4 +1,4 @@
-package edu.nlp.finalproj;
+package edu.finalproj;
 
 
 
@@ -8,8 +8,13 @@ package edu.nlp.finalproj;
 
 import javax.swing.*;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
 
@@ -17,25 +22,40 @@ import java.util.Random;
  * This class exists simply to play around with Java's 2DGraphics library
  * so that eventually we'll be able to integrate it into the final project
  */
-public class GraphicsTester extends JPanel implements ActionListener {
+public class GraphicsApplet extends JPanel implements ActionListener {
 		
-	
-	// Strings (For labelling radio buttons, and also internal not!enums).
+
+
 	static String sentimentStr = "Good V. Bad";
 	static String stemStr = "Stemming";
-	static String etyStr = "Etymology";	
+	static String etyStr = "Etymology";
+	static String runStr = "Run TextVisualizer";
+	static String loadStr = "Load Book";
+	
+	WordNet wordnet; 
+	PDDocument book;
 	
 	int pageGrain = 500; //specifies how many expected words per page
 	int bookGrain = 1000; //specifies how many expected pages per book
 	
+	//These dont' have to be variables, but it makes the communication
+	// easier than using tags on objects in the container....
 	String fileName;
 	String genType;
+	JButton but_run;
 	JTextField source;
 	JPanel paint;
 	RepaintManager painter;
 	
-	public GraphicsTester(){
-		
+	public GraphicsApplet(){
+		this.display();
+	};
+	
+	public GraphicsApplet(WordNet wn){
+		this.display();
+	}
+	
+	public void display(){
 		//Setting up the radio first, because it takes the most code. 
 		
 		//individual buttons. 
@@ -64,8 +84,12 @@ public class GraphicsTester extends JPanel implements ActionListener {
 		//Also, the file filter and the 'run' command button.
 		source = new JTextField(20);
 		source.setActionCommand("File");
-		JButton button = new JButton("Run TextVisualizer");
-		button.setActionCommand("Run");
+		but_run = new JButton(runStr);
+		but_run.setActionCommand(runStr);
+		but_run.setEnabled(false);
+		
+		JButton but_load = new JButton(loadStr);
+		but_load.setActionCommand(loadStr);
 		
         JPanel radioPanel = new JPanel( new GridLayout(0,1));
         JPanel sourcePanel = new JPanel();
@@ -78,10 +102,11 @@ public class GraphicsTester extends JPanel implements ActionListener {
 		
 		sourcePanel.add(new JLabel("Input File Name:"));
 		sourcePanel.add(source);
+		sourcePanel.add(but_load);
 		
 		selectPanel.add(radioPanel);
 		selectPanel.add(sourcePanel);
-		selectPanel.add(button);
+		selectPanel.add(but_run);
 
 		this.setLayout(new GridLayout(2,1));
 		add(paint, BorderLayout.CENTER); // Puts the painting object at the top of our applet;
@@ -90,20 +115,35 @@ public class GraphicsTester extends JPanel implements ActionListener {
 		rad_goodbad.addActionListener(this);
 		rad_stem.addActionListener(this);
 		rad_ety.addActionListener(this);
-		button.addActionListener(this);
-		source.addActionListener(this); //Current janky implementation requires hitting enter. Fix?
-	}
+		but_run.addActionListener(this);
+		but_load.addActionListener(this);
+		source.addActionListener(this);
+		}
 	
-	public void actionPerformed(ActionEvent e){
-		if (e.getActionCommand().equals("File")){
+	public void actionPerformed(ActionEvent event){
+		if (event.getActionCommand().equals("File")){
 			fileName = source.getText();
-		} else if (e.getActionCommand().equals(sentimentStr)){
+		} else if (event.getActionCommand().equals(loadStr)){
+			fileName = source.getText();
+		// create BufferedReader
+        try {
+            book = PDDocument.load(new File(fileName));
+            bookGrain = book.getNumberOfPages();
+            
+            but_run.setEnabled(true);
+        } catch (IOException e) { //
+        	// if file not found don't care but don't let us run. 
+        	System.out.println("Can't find file: ");
+        	System.out.println(fileName);
+        };
+			
+		} else if (event.getActionCommand().equals(sentimentStr)){
 			genType = sentimentStr;
-		} else if (e.getActionCommand().equals(stemStr)){
+		} else if (event.getActionCommand().equals(stemStr)){
 			genType = stemStr;
-		} else if (e.getActionCommand().equals(etyStr)){
+		} else if (event.getActionCommand().equals(etyStr)){
 			genType = etyStr;
-		} else if (e.getActionCommand().equals("Run")){
+		} else if (event.getActionCommand().equals(runStr)){
 			// Call main implementation
 			// and make sure there or here we catch for file errors!
 			paintBook(paint);
@@ -118,33 +158,46 @@ public class GraphicsTester extends JPanel implements ActionListener {
 		//while in bounds
 		int maxX = paint.getWidth();
 		int maxY = paint.getHeight();
-		int curX = 0;
+		int curPage = 0, curWord = 0, curX = 0, curY = 0;
 		int xShift = maxX/bookGrain+1; //we define how large our rectangles are
-		int yShift = maxY/pageGrain+1; // by the book/page granularity, offset by one for int division.
-
+	
 		Random rand = new Random(); //for testing
-		
-		while (curX < maxX){
-			int curY = 0;
+		PDFTextStripper stripper;
+		while (curPage < bookGrain){ //while on page i
+			
+			//reset iterators for internal loop
+			curY = 0;
+			curWord = 0; 
+			
+			try { 
+				stripper = new PDFTextStripper();
+	            stripper.setStartPage(curPage);
+	            stripper.setEndPage(curPage);
+	
+	            String[] page = stripper.getText(book).split(" ");
+				pageGrain = page.length;
+				int yShift = maxY/pageGrain+1; // by the book/page granularity, offset by one for int division.
 
-			while (curY < maxY){
-				int[] xs = {curX, curX, curX+xShift, curX+xShift}; 
-				int[] ys = {curY, curY+yShift,curY+yShift, curY};
-				
-				
-				 //for testing
-				Float zero = new Float(0.0);
-				if ( genType.equals(stemStr)){
-					canvas.setColor(new Color(zero, rand.nextFloat(), zero));
-				} else if (genType.equals(etyStr)){
-					canvas.setColor(new Color(zero,  zero, rand.nextFloat()));
-				} else { //if genType.equals(Sentiment){ 
-					canvas.setColor(new Color(rand.nextFloat(), zero,zero));
-				}
-				canvas.fillPolygon(xs, ys, 4);
-				curY+=yShift+1;
-			};
+				while (curWord < pageGrain){
+					int[] xs = {curX, curX, curX+xShift, curX+xShift}; 
+					int[] ys = {curY, curY+yShift,curY+yShift, curY};
+					
+					Float zero = new Float(0.0);
+					if ( genType.equals(stemStr)){
+						//double[] colors = wordnet.getColors();
+						canvas.setColor(new Color(zero, rand.nextFloat(), zero));
+					} else if (genType.equals(etyStr)){
+						canvas.setColor(new Color(zero,  zero, rand.nextFloat()));
+					} else { //if genType.equals(Sentiment){ 
+						canvas.setColor(new Color(rand.nextFloat(), zero,zero));
+					}
+					canvas.fillPolygon(xs, ys, 4);
+					curY+=yShift+1;
+					//curWord += 1;
+				};
+			} catch (IOException e) { e.printStackTrace(); }
 			curX+=xShift+1;
+			curPage+=1;
 		};
 		
 		
@@ -158,7 +211,7 @@ public class GraphicsTester extends JPanel implements ActionListener {
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
-		GraphicsTester panel = new GraphicsTester();
+		GraphicsApplet panel = new GraphicsApplet();
 		application.add(panel);
 		application.pack();
 
