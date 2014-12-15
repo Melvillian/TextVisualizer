f74daa6e34833715bda4aa9d9960762d2a4b7297
Mari Bennett <mbennett@hmc.edu>
2014-12-15 14:52:35 -0800

    Added parsing coloring and normalization to drawing application, added getters to tuple class. Also tweaked a few applet things: like how textfield was being read.
    Signed-off-by: Mari Bennett <mbennett@hmc.edu>

--- edu/finalproj/GraphicsApplet.java
+++ edu/finalproj/GraphicsApplet.java
@@ -7,6 +7,10 @@ package edu.finalproj;
  */
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
@@ -15,25 +19,26 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
-
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * This class exists simply to play around with Java's 2DGraphics library
  * so that eventually we'll be able to integrate it into the final project
  */
-public class GraphicsApplet extends JPanel implements ActionListener {
+public class GraphicsApplet extends JPanel implements ActionListener, ItemListener, DocumentListener {
 		
-
-
+	private static final long serialVersionUID = -4200617947533784654L; //Just to get Eclipse to shush.
+	
 	static String sentimentStr = "Good V. Bad";
-	static String stemStr = "Stemming";
-	static String etyStr = "Etymology";
+	static String stemStr = "Parsing";
+	static String normStr = "Use Normalized Coloring";
+	static String blockStr = "Use Word Precision";
 	static String runStr = "Run TextVisualizer";
 	static String loadStr = "Load Book";
 	
 	WordNet wordnet; 
+	StanfordParser sp;
 	PDDocument book;
 	
 	int pageGrain = 500; //specifies how many expected words per page
@@ -46,7 +51,13 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 	// easier than using tags on objects in the container....
 	String fileName;
 	String genType;
-	String gramType = "page"; // I still need to make a radio button for this.
+	String gramType = "page";
+	
+	JCheckBox check_norm;
+	JCheckBox check_block;
+	boolean flag_norm;
+	boolean flag_block;
+	
 	JButton but_run;
 	JTextField source;
 	JPanel paint;
@@ -57,11 +68,13 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 	
 	public GraphicsApplet(){
         this.wordnet = new WordNet("SentiWordNet.txt");
+        this.sp  = new StanfordParser();
 		this.display();
 	};
 	
 	public GraphicsApplet(WordNet wn){
 		this.wordnet = wn;
+		this.sp = new StanfordParser();
 		this.display();
 	}
 	
@@ -77,14 +90,15 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 		JRadioButton rad_stem = new JRadioButton(stemStr);
 		rad_stem.setActionCommand(stemStr);
 		
-		JRadioButton rad_ety = new JRadioButton(etyStr);
-		rad_ety.setActionCommand(etyStr);
-		
 		//then we add them to the button group
 		ButtonGroup group = new ButtonGroup();
 		group.add(rad_goodbad);
 		group.add(rad_stem);
-		group.add(rad_ety);
+		
+		//We need to define two check boxes: one for globalized normalization
+		// and the other for determining page or ngram based images.
+		check_norm = new JCheckBox(normStr);
+		check_block = new JCheckBox(blockStr);
 		
 		// The image panel must be defined as well
 		paint = new JPanel();
@@ -93,7 +107,7 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 
 		//Also, the file filter and the 'run' command button.
 		source = new JTextField(20);
-		source.setActionCommand("File");
+		source.getDocument().addDocumentListener(this);
 		but_run = new JButton(runStr);
 		but_run.setActionCommand(runStr);
 		but_run.setEnabled(false);
@@ -104,18 +118,23 @@ public class GraphicsApplet extends JPanel implements ActionListener {
         JPanel radioPanel = new JPanel( new GridLayout(0,1));
         JPanel sourcePanel = new JPanel();
         JPanel selectPanel = new JPanel();
+        JPanel runPanel = new JPanel( new GridLayout(0,1) );
         
         radioPanel.add(new JLabel("Selection Method:"));
 		radioPanel.add(rad_goodbad);
 		radioPanel.add(rad_stem);
-		radioPanel.add(rad_ety);
 		
 		sourcePanel.add(new JLabel("Input File Name:"));
 		sourcePanel.add(source);
 		sourcePanel.add(but_load);
 		
+		runPanel.add(check_block);
+		runPanel.add(check_norm);
+		runPanel.add(sourcePanel);
+		
 		selectPanel.add(radioPanel);
 		selectPanel.add(sourcePanel);
+		selectPanel.add(runPanel);
 		selectPanel.add(but_run);
 
 		this.setLayout(new GridLayout(2,1));
@@ -124,47 +143,135 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 		
 		rad_goodbad.addActionListener(this);
 		rad_stem.addActionListener(this);
-		rad_ety.addActionListener(this);
 		but_run.addActionListener(this);
 		but_load.addActionListener(this);
 		source.addActionListener(this);
+		
+		check_norm.addItemListener(this);
+		check_block.addItemListener(this);
 		}
 	
+	public void itemStateChanged(ItemEvent e){
+		Object source = e.getItemSelectable();
+		
+		if (source == check_norm){
+			flag_norm = (e.getStateChange() == ItemEvent.SELECTED) ? true : false;
+		} else if (source == check_block){
+			flag_block = (e.getStateChange() == ItemEvent.SELECTED) ? true : false;
+		};
+	}
+	
+	public void changedUpdate(DocumentEvent e){
+		Document doc = e.getDocument();
+		try {
+			fileName = doc.getText(0,doc.getLength());
+		} catch (BadLocationException e1) {
+			//
+		}
+	};
+	
+	public void insertUpdate(DocumentEvent e){
+		Document doc = (Document)e.getDocument();
+		try {
+			fileName = doc.getText(0,doc.getLength());
+		} catch (BadLocationException e1) {
+			//
+		}
+	};
+	public void removeUpdate(DocumentEvent e){
+		Document doc = (Document)e.getDocument();
+		try {
+			fileName = doc.getText(0,doc.getLength());
+		} catch (BadLocationException e1) {
+			//
+		}
+	};
+	
 	public void actionPerformed(ActionEvent event){
-		if (event.getActionCommand().equals("File")){
-			fileName = source.getText();
-		} else if (event.getActionCommand().equals(loadStr)){
-			fileName = source.getText();
-		// create BufferedReader
-        try {
-            book = PDDocument.load(new File(fileName));
-            bookGrain = book.getNumberOfPages();
-            
-            but_run.setEnabled(true);
-        } catch (IOException e) { //
-        	// if file not found don't care but don't let us run. 
-        	System.out.println("Can't find file: ");
-        	System.out.println(fileName);
-        };
-			
+		
+		if (event.getActionCommand().equals(loadStr)){
+			// create BufferedReader
+	        try {
+	            book = PDDocument.load(new File(fileName));
+	            bookGrain = book.getNumberOfPages();
+	            but_run.setEnabled(true);
+	        } catch (IOException e) { //
+	        	// if file not found don't care but don't let us run. 
+	        	System.out.printf("Can't find file: %s.\n", fileName);
+	        };
+	        
+	    //These three handle the radio  button (for now).
 		} else if (event.getActionCommand().equals(sentimentStr)){
 			genType = sentimentStr;
 		} else if (event.getActionCommand().equals(stemStr)){
-			genType = stemStr;
-		} else if (event.getActionCommand().equals(etyStr)){
-			genType = etyStr;
+			genType = stemStr;			
+		//Then, if we trigger the run event, we should run!
 		} else if (event.getActionCommand().equals(runStr)){
-			// Call main implementation
-			// and make sure there or here we catch for file errors!
-			paintBook(paint);
-			try{
-				book.close();
-			} catch (IOException e){};
+			
+			negCount = 0;
+			posCount = 0;
+			if (genType.equals(stemStr)){
+				paintPage(paint, 2, 3);
+			}
+			if (flag_norm){
+				float[] minmax = analyzeBook();
+				paintBook(paint, minmax[0], minmax[1]);
+			} else {
+				paintBook(paint, -1f, 1f);
+			};
+			
+			System.out.printf("Using the method %s, the following counts were found:\n", gramType);
+			System.out.printf("Positive Blocks (Blue): %d\n", posCount);
+			System.out.printf("Negative Blocks (Red): %d\n", negCount);
+			
+//			try{ //And, because of IO, we have to try to close the pdf. 
+//				book.close();
+//			} catch (IOException e){};
 		}
  
 	}
+
+
+	public void paintPage(JPanel panel, int first, int last){
+		Graphics canvas = panel.getGraphics();
+		canvas.setPaintMode();
+		
+		//while in bounds
+		int maxX = paint.getWidth();
+		int maxY = paint.getHeight();
+		
+		PDFTextStripper stripper;
+		try {
+			stripper = new PDFTextStripper();
+	        stripper.setStartPage(first);
+	        stripper.setEndPage(last);
+	        
+	        ArrayList< ArrayList<Tuple> > pageParse = sp.parseText(stripper.getText(book));
+
+			int xShift = maxX/pageParse.size(); //we define how large our bars are
+			int curX = 0;
+			
+	        for (ArrayList<Tuple> senParse : pageParse){
+	        	int yShift = maxY/senParse.size();
+	        	int curY = 0;
+	        	
+	        	for (Tuple phrase : senParse){
+	        		int wc = phrase.getCnt()*yShift;
+
+					int[] xs = {curX, curX, curX+xShift, curX+xShift}; 
+					int[] ys = {curY, curY+wc,curY+wc, curY};
+									
+					canvas.setColor(phrase.getColor() );
+					canvas.fillPolygon(xs, ys, 4);
+					curY+=wc;
+	        	};
+	        curX+=yShift; 
+	        };
+		} catch (IOException e1) {};
+	};
 	
-	public void paintBook(JPanel panel){
+
+	public void paintBook(JPanel panel, float min, float max){
 		Graphics canvas = panel.getGraphics();
 		canvas.setPaintMode();
 		
@@ -173,10 +280,9 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 		int maxY = paint.getHeight();
 		int curPage = 0, curWord = 0, curX = 0, curY = 0;
 		int xShift = maxX/bookGrain+1; //we define how large our rectangles are
-	
-		Random rand = new Random(); //for testing
+
 		PDFTextStripper stripper;
-		while (curPage < bookGrain){ //while on page i
+		while (curPage <= bookGrain){ //while on page i
 			
 			//reset iterators for internal loop
 			curY = 0;
@@ -192,8 +298,7 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 				pageGrain = page.length;
 				if (pageGrain != 0){
 					
-					if (gramType.equals("ngram")){
-						//If we're using ngram based granularity 
+					if (flag_block){ //If we're using ngram based granularity 
 						int yShift = maxY/pageGrain+1; // by the book/page granularity, offset by one for int division.
 						while (curWord < pageGrain - 4 ){
 							int[] xs = {curX, curX, curX+xShift, curX+xShift}; 
@@ -203,27 +308,20 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 																	page[curWord+2], page[curWord+3]);
 								curWord += wordColor.getWC();
 								double[] colors = wordColor.get();
-							if ( genType.equals(stemStr)){
-								canvas.setColor(new Color( (float)0.0, (float)colors[0], (float)colors[1]));
-							} else if (genType.equals(etyStr)){
-								canvas.setColor(new Color( (float)colors[0], (float)colors[1], (float)0.0));
-							} else { //if genType.equals(Sentiment){ 
+						//	if (genType.equals(sentimentStr)){ 
 								float neg = (float)colors[1];
 								float pos = (float)colors[0];
-								float abs = Math.abs(neg+pos);
-								neg = neg/abs;
-								pos = pos/abs; 
+								float val = (flag_norm) ? (pos-neg)/(2*(max-min)) : (pos-neg)/(2*(pos+neg)); //this scales according to worst and best we've seen. 
 								if (neg > pos){
-									canvas.setColor(new Color( 0.5f, 0.5f*(1-neg), 0.5f*(1-neg) ));
+									canvas.setColor(new Color( val, val, 0 ));
 									negCount++;
 								} else if (pos > neg ) {
-									canvas.setColor(new Color( 0.5f, 0.5f*(1+pos), 0.5f*(1+pos) ));
+									canvas.setColor(new Color( 0, val, val ));
 									posCount++;
 								} else {
 									canvas.setColor(new Color( 0.5f, 0.5f, 0.5f));
 								}
 							
-							}
 							canvas.fillPolygon(xs, ys, 4);
 							curY+=yShift;
 						};
@@ -237,18 +335,18 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 								curWord += wordColor.getWC();
 								double[] newcolors = wordColor.get();
 								colors[0] += newcolors[0];
+								
 								colors[1] += newcolors[1];
 							}
-							float neg = (float)colors[1];
-							float pos = (float)colors[0];
-							float abs = Math.abs(neg+pos);
-							neg = neg/abs;
-							pos = pos/abs; 
+						float neg = (float)colors[1];
+						float pos = (float)colors[0];
+						
+						float val = (flag_norm) ? (pos-neg)/(2*(max-min)) : (pos-neg)/(2*(pos+neg)); //this scales according to worst and best we've seen. 
 							if (neg > pos){
-								canvas.setColor(new Color( 0.5f, 0.5f*(1-neg), 0.5f*(1-neg) ));
+								canvas.setColor(new Color( val+0.5f, 0, 0 ));
 								negCount++;
 							} else if (pos > neg ) {
-								canvas.setColor(new Color( 0.5f, 0.5f*(1+pos), 0.5f*(1+pos) ));
+								canvas.setColor(new Color( 0, val+0.5f, val+0.5f));
 								posCount++;
 							} else {
 								canvas.setColor(new Color( 0.5f, 0.5f, 0.5f));
@@ -260,13 +358,59 @@ public class GraphicsApplet extends JPanel implements ActionListener {
 			} catch (IOException e) { e.printStackTrace(); }
 			curX+=xShift;
 			curPage+=1;
-		};
+		};		
+	}
+	
+	public float[] analyzeBook(){
+		PDFTextStripper stripper;
+		float min = 0;
+		float max = 0;
+		
+		int curPage = 0;
+		while (curPage <= bookGrain){ //while on page i
+			try { 
+				stripper = new PDFTextStripper();
+	            stripper.setStartPage(curPage);
+	            stripper.setEndPage(curPage);
+	            int curWord = 0;
+	            
+	            String[] page = stripper.getText(book).split("[^A-Za-z]+");
+				pageGrain = page.length;							
+					if (flag_block){
+						while (curWord < pageGrain - 4  ){
+							SentiWord wordColor = wordnet.test(page[curWord], page[curWord+1], 
+									page[curWord+2], page[curWord+3]);
+							
+							curWord+=wordColor.getWC();
+							double[] colors = wordColor.get();
+							double val = colors[0]-colors[1];
+							
+							if (val < min){ min = (float)val; }
+							if (val > max){ max = (float)val; }
+						};
+					} else {
 
-		System.out.printf("Using the method %s, the following counts were found:\n", gramType);
-		System.out.printf("Positive Blocks (Blue): %d\n", posCount);
-		System.out.printf("Negative Blocks (Red): %d\n", negCount);
-		
-		
+						double[] colors = {0.0, 0.0}; 
+						while (curWord < pageGrain - 4  ){
+							SentiWord wordColor = wordnet.test(page[curWord], page[curWord+1], 
+									page[curWord+2], page[curWord+3]);
+							
+							curWord+=wordColor.getWC();
+							double[] newcolors = wordColor.get();
+							colors[0] += newcolors[0];
+							colors[1] += newcolors[1];
+						};
+						double val = colors[0]-colors[1];
+						if (val < min){ min = (float)val; }
+						if (val > max){ max = (float)val; }
+					};
+						
+			} catch (IOException e){
+				//
+			};
+			curPage++;
+		};
+		return new float[]{min, max};
 	}
 	
 	public static void main(String[] args){
--- edu/finalproj/Tuple.java
+++ edu/finalproj/Tuple.java
@@ -1,5 +1,7 @@
 package edu.finalproj;
 
+import java.awt.Color;
+
 /**
  * Created by alex on 12/11/14.
  */
@@ -7,9 +9,42 @@ public class Tuple {
     public String pos;
     public int cnt;
 
-
     public Tuple(String pos, int cnt) {
         this.pos = pos;
         this.cnt = cnt;
+    }
+    
+    public String getPOS(){
+    	return this.pos;
+    }
+    
+    public int getCnt(){
+    	return this.cnt;
+    }
+    
+
+    public Color getColor(){
+    	if (pos.equals("MISC")){
+    		return Color.GRAY;
+    	}
+    	else if (pos.equals("PREP")){
+    		return Color.PINK;
+    	}
+    	else if (pos.equals("DET")){
+    		return Color.LIGHT_GRAY;    		
+    	}
+    	else if (pos.equals("ADJECT")){
+    		return Color.BLUE;
+    	}
+    	else if (pos.equals("NOUNS")){
+    		return Color.GREEN;
+    	}
+    	else if (pos.equals("ADVERBS")){
+    		return  Color.MAGENTA;
+    	}
+    	else if (pos.equals("VERBS")){
+    		return  Color.RED;
+    	}
+    	else return Color.WHITE;
     }
 }
